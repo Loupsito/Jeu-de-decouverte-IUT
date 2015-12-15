5cdcd69ed2fb5af894c9668f16d9d68575a63b60
Alexandre Suy <virya.suy@gmail.com>
2015-12-15 20:25:36 +0100

    5eme version : 	- Gros ménage dans le code 	- Séparation entre : 		- le JavaScript 		- le fichier php qui contient les tab et variable php 		- le fichier html qui concentre le tout

---
+++ pageAffichage.js
@@ -0,0 +1,282 @@
+	
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+					
+var tampon;
+
+//Les actions
+var manger = {"nomAction" : "manger","prerequis" : ["joueur.idSalle==0","verifPossessionItem('cle') == true"]};
+var ecraser = {"nomAction": "ecraser","prerequis" : ["joueur.idSalle==0"]};
+
+//Le tableau qui contient les actions
+var listesActions = [ecraser,manger];	
+			
+//Fonction qui permet de creer la balise que l'on veut (<p>,<span>,etc.)
+function genereContenu(element,contenu,divMere)
+{
+    nouveauDiv = document.createElement(element);			  //creation de l'element
+    nouveauDiv.innerHTML = contenu							  //Attribution d'un contenu
+    document.getElementById(divMere).appendChild(nouveauDiv); //pour insérer dans une div qu'on aura donnee au prealable
+}
+function genereContenuID(element,contenu,divMere,idd)
+{
+    nouveauDiv = document.createElement(element);			  //creation de l'element
+    nouveauDiv.innerHTML = contenu							  //Attribution d'un contenu
+    nouveauDiv.id=idd;
+    document.getElementById(divMere).appendChild(nouveauDiv); //pour insérer dans une div qu'on aura donnee au prealable
+}
+					
+//---------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------DEPLACEMENT--------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+					
+//classe joueur pour connaître la position de l'utilisateur
+function Joueur (idSalle)
+{
+    //salle
+    this.idSalle = idSalle || 2;
+    //orientation du joueur
+}
+									
+//fonction générant le texte et les boutons
+function genererTexte()
+{
+    //position du joueur
+    var positionJoueur = '<b>Position du joueur =></b> '+listeCases[joueur.idSalle][1];
+    genereContenu('p',positionJoueur,'deplacement');
+  
+    //affichage des boutons de scènes (portes ou longs couloirs)
+    var boutonsDeplacement = '<b>Salles adjacentes :</b><br/>';
+    genereContenu('p',boutonsDeplacement,'deplacement'); 
+					
+    for (i = 0; i < listeCases[joueur.idSalle][2].length; i++)
+    {
+        //affiche le nom de la scène dans les boutons créés.
+	genereContenu('span','<button type="button" onclick="avancer(listeCases[joueur.idSalle][2]['+i+'][0],'+i+')">'+listeCases[joueur.idSalle][2][i][1]+'</button>','deplacement');
+    } 
+						
+
+						
+    //affiche boutons items  
+    var caseInventaire = '<b>Objets que vous voyez :</b><br/>';
+    genereContenu('p',caseInventaire,'deplacement'); 
+    for (i = 0; i < listeCases[joueur.idSalle][3].length; i++)
+    {
+        genereContenu('span','<button type="button">'+listeCases[joueur.idSalle][3][i]+'</button>','objet');
+    }
+						
+    //-----------------------------------------------------------------------------------------------------------------------------
+    //----------------------------------------------Analyse-des-boutons-et-traitement----------------------------------------------
+    var	captureBouton = document.querySelectorAll('#objet span');
+    document.getElementById('objet').innerHTML = "";
+    for(var i = 0; i<captureBouton.length;i++)
+    {
+        //genereContenu('span','<button type="button" onclick="selectionObjet('+"'"+captureBouton[i].textContent+"'"+')">'+captureBouton[i].textContent+'</button>','objet');
+	placementItem(captureBouton[i].textContent);
+    }
+						//-----------------------------------------------------------------------------------------------------------------------------
+						//-----------------------------------------------------------------------------------------------------------------------------
+						
+					  
+}						
+				
+//Fonction qui fait avancer le joueue                                
+function avancer(newScene,indice)
+{	
+//test la case lointaine est accessible
+
+        //affiche une alerte si non accessible
+        if (listeCases[joueur.idSalle][2][indice][2] == false)			
+                alert("La porte est vérrouillé, il doit y avoir une cle près d\'ici...");
+        //case lointaine accessible => le joueur peut avancer
+        else 
+                joueur.idSalle = newScene;
+
+        document.getElementById('deplacement').innerHTML = "";
+        document.getElementById('objet').innerHTML = "";
+        genererTexte();
+}
+					
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------INTERACTION-------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+
+  //Fonction qui change le contenu de l'ecran : affiche les actions
+function changementAff(val)
+{ 
+        for(var i = 0; i<tabDeTousLesItems.length;i++)
+        {
+                if (tabDeTousLesItems[i][0] == val)
+                {
+                        //On suvegarde la valeur de l'objet pour pouvoir l'utiliser plus tard
+                        tampon = val;
+                        //on supprime tous le contenu de la division 'objet'
+                        document.getElementById('objet').innerHTML = "";
+                        //puis on ajoute nos nouveaux elements
+                        genereAction(tabDeTousLesItems[i][1][0]); 
+                        break;
+                }
+                else
+                {
+                        document.getElementById('objet').innerHTML = "<h1>Vous avez "+val+" la "+tampon+"!</h1>";
+                }
+        }
+
+        var captureBouton = document.querySelectorAll('#objet span');
+        document.getElementById('objet').innerHTML = "";
+        for(var i = 0; i<captureBouton.length;i++)
+        {
+                //parcours le tableau d'actions
+                for(var j = 0; j <listesActions.length;j++)
+                {
+                        //compare les chaines de caractères contenues dans le tableau d'objet et celui des actions
+                        if (captureBouton[i].textContent == listesActions[j]["nomAction"])
+                        {
+                                verifiePrerequis(listesActions[j]["nomAction"]);
+                        }
+                }
+        }
+}
+
+function afficheResultat(val)
+{
+        document.getElementById('objet').innerHTML = "<h1>Vous avez "+val+" la "+tampon+"!</h1>";
+}
+
+// Efface que la première occurrence pour la valeur
+function arrayUnsetByValue(array, value)
+{ 
+        array.splice(array.indexOf(value), 1);
+}
+
+//Verifie si l'item est dans l'inventairer ou pas
+function verifPossessionItem(leItem)
+{
+        for(var i = 0; i<tabDeTousLesItems.length;i++)
+        {
+                if (tabDeTousLesItems[i][0] == leItem)
+                {
+                        return tabDeTousLesItems[i][1][3];
+                }
+        }
+}
+
+//Place l'item en fonction de son booleen
+function placementItem(leItem)
+{
+        if (verifPossessionItem(leItem) == true)//Si c'est dans l'inventaire
+        {
+                //ne rien faire
+        }
+        else 									//Si c'est dans la salle
+        {
+                genereContenuID('span','<button type="button" onclick="selectionObjet('+"'"+leItem+"'"+')">'+leItem+'</button>','objet',leItem);
+        }	
+}
+
+//Place l'item dans l'inventaire si celui-ci est a false
+function placementItemDansInventaire(leItem)
+{
+        if (verifPossessionItem(leItem) == true)            //Si c'est dans l'inventaire
+        {
+                genereContenuID('span','<button type="button" onclick="changementAff('+"'"+leItem+"'"+')">'+leItem+'</button>','inventaire',leItem);
+        }
+        else                                                 //Si c'est dans la salle
+        {
+                genereContenuID('span','<button type="button" onclick="selectionObjet('+"'"+leItem+"'"+')">'+leItem+'</button>','objet',leItem);
+        }	
+}
+
+//Fonction utilise lors de la selection d'un item
+function selectionObjet(leItem)
+{
+        for(var i = 0; i<tabDeTousLesItems.length;i++)
+        {
+                if (tabDeTousLesItems[i][0] == leItem)
+                {
+                        tabDeTousLesItems[i][1][3] = true;//true ou false
+                }
+                else
+                {
+                        //ne rien faire
+                }
+        }	
+        //suppresion de l'item
+        var monItem = document.getElementById(leItem);
+        monItem.parentNode.removeChild(monItem);
+
+        placementItemDansInventaire(leItem);
+}
+
+//Analyse les items a true et les places dans l'inventaire si c'est le cas
+function debutInventaire()
+{
+        for(var i = 0; i<tabDeTousLesItems.length;i++)
+                {
+                        if (tabDeTousLesItems[i][1][3] == true)
+                        {
+                                genereContenuID('span','<button type="button" onclick="changementAff('+"'"+tabDeTousLesItems[i][0]+"'"+')">'+tabDeTousLesItems[i][0]+'</button>','inventaire',tabDeTousLesItems[i][0]);
+                        }
+                        else
+                        {
+                                //ne rien faire
+                        }
+                }	
+}
+
+//Fonction verifiant les prerequis des actions
+function verifiePrerequis(action)
+{
+        var erreurs = 0 ;
+        for(i=0;i<listesActions.length;i++)
+        {
+                if (listesActions[i]['nomAction']==action)//identification du nom de l'action
+                        {
+                                //alert(listesActions[i]['nomAction']+" = "+action);
+                                for(j=0;j<listesActions[i]['prerequis'].length;j++)//verification validation prerequis
+                                {
+                                        if (eval(listesActions[i]['prerequis'][j]))
+                                        {
+                                                //alert("ACTION = "+listesActions[i]['nomAction']+"    Position :  ==>"+listesActions[i]['prerequis'][j]+"  : prerequis respecte");
+                                        }
+                                        else
+                                        {	
+                                                alert("ACTION = "+listesActions[i]['nomAction']+"    Position :  ==>"+listesActions[i]['prerequis'][j]+"  :prerequis non respecte");
+                                                erreurs +=1;
+                                        }
+                                }
+                                if (erreurs != 0)
+                                        alert(erreurs+" prerequis pas respecte pour " +listesActions[i]['nomAction']);
+                                else
+                                {
+                                        //alert("Affichage des actions pour " +listesActions[i]['nomAction']);
+                                        genereContenu('span','<button type="button" onclick="afficheResultat('+"'"+listesActions[i]['nomAction']+"'"+')">'+listesActions[i]['nomAction']+'</button>','objet');
+                                }	
+                        }
+                else
+                        {
+                                //alert(listesActions[i]['nomAction']+" =/= "+action + "  :  NOPE");
+                        }
+        }
+}
+
+//Fontion qui genere des actions en fonction d'un item
+function genereAction(tabActions)
+{
+        document.getElementById('objet').innerHTML = "";
+        for(var i=0;i < tabActions.length;i++)
+        {	
+                genereContenu('span','<button type="button" onclick="">'+tabActions[i]+'</button>','objet');
+        }
+}
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------
+
+//Analyse les items a true et les places dans l'inventaire si c'est le cas
+debutInventaire();
+//instanciation du joueur
+var joueur = new Joueur(); 					
+//gerere le texte et les boutons
+genererTexte();		
--- textes.php
+++ textes.php
@@ -1,74 +1,48 @@
 <?php
-//------------------------------------------------------------LES-SALLES------------------------------------------------------------
-
 //salle G25
-			$g25Nord = "Il y a des fenêtres en face de vous, vous voyez le vent souffler sur les branches d'arbres dehors...";
-			$g25Ouest = "Vous voyez des postes et des tables en face de vous, tout au fond un mur.";
-			$g25Sud = "En face de vous se trouve une porte qui mène vers le couloir...";
-			$g25Est = "Il y a des postes et des tables en face de vous. Au fond, un tableau blanc et un bureau, surement celui du professeur...";
-		$g25Orientation = array($N = array(0,null,$g25Nord,false, $objet= array()),$O = array(1,null,$g25Ouest,false, $objet= array("brosse")),$S = array(2,1,$g25Sud,true, $objet= array()),$E = array(3,null,$g25Est,false, $objet= array("crayon","pomme")));
-	$g25 = array(0, "G25", $g25Orientation); 	
-						
+  $g25scenesAdjacentes = array($scene1 = array(1,"couloir 1",true),$scene2 = array(3,"I21",false));
+ $g25 = array(0, "G25", $g25scenesAdjacentes, $item= array("cle","pomme","brosse"));  
+
 //couloir 1
-			$couloir1Nord = "En face de vous, une porte menant à la salle G25...";
-			$couloir1Ouest = "Un long couloir s'étend en face de vous, vous pouvez avancer...";
-			$couloir1Sud = "Il y a un mur en face de vous, rien de bien intéressant, pivotez...";
-			$couloir1Est = "Vous voyez une intersection, cependant, une force mysterieuse vous empêche d'avancer dans cette direction, pivotez.";						
-		$couloir1Orientation = array($N = array(0,0,$couloir1Nord,true, $objet= array()),$O = array(1,2,$couloir1Ouest,true, $objet= array()),$S = array(2,null,$couloir1Sud,false, $objet= array()),$E = array(3,null,$couloir1Est,false, $objet= array()));
-	$couloir1 = array(1, "couloir1", $couloir1Orientation); 					
-	
+  $couloir1scenesAdjacentes = array($scene1 = array(0,"G25",true),$scene2 = array(2,"couloir 2",true));
+ $couloir1 = array(1, "couloir1", $couloir1scenesAdjacentes, $item= array());      
+
 //couloir 2
-			$couloir2Nord = "En face de vous, une porte menant à la salle I21...";
-			$couloir2Ouest = "Un long couloir s'étend en face de vous. Une force inconnue vous empêche d'avancer dans cette direction...";
-			$couloir2Sud = "Il y a un mur en face de vous, des affiches du prochain concert de Virya y sont affichées...";
-			$couloir2Est = "Un couloir s'étend en face de vous, vous pourrez aller en salle g25 si vous avancez...";							
-		$couloir2Orientation = array($N = array(0,3,$couloir2Nord,false, $objet= array()),$O = array(1,null,$couloir2Ouest,false, $objet= array()),$S = array(2,null,$couloir2Sud,false, $objet= array()),$E = array(3,1,$couloir2Est,true, $objet= array()));
-	$couloir2 = array(2, "couloir2", $couloir2Orientation); 	
+  $couloir2scenesAdjacentes = array($scene1 = array(1,"couloir 1",true),$scene2 = array(3,"I21",false));
+ $couloir2 = array(2, "couloir2", $couloir2scenesAdjacentes, $item= array());  
 
 //salle I21
-			$i21Nord = "Il y a des fenêtres fermées et un radiateur en face de vous. Les radiateurs semblent être allumés, il fait chaud...";
-			$i21Ouest = "Vous voyez tables en face de vous, tout au fond un mur affichant en gros 'I21'.";
-			$i21Sud = "En face de vous se trouve une porte qui mène vers le couloir...";
-			$i21Est = "Il y a des postes et des tables en face de vous. Au fond, un tableau blanc et un bureau, surement celui du professeur...";							
-		$i21Orientation = array($N = array(0,null,$i21Nord,false, $objet= array()),$O = array(1,null,$i21Ouest,false, $objet= array()),$S = array(2,2,$i21Sud,false, $objet= array()),$E = array(3,null,$i21Est,false, $objet= array()));
-	$i21 = array(3, "I21", $i21Orientation); 														
-						
-//ensemble des actions
-	$ouvrirPorte = array("cle = true","porte = false","case != null");
-	$pivoter = array();
-																		
-$listeAction = array("ouvrirPorte" => $ouvrirPorte);											
+  $i21scenesAdjacentes = array($scene1 = array(0,"G25",true),$scene2 = array(2,"couloir 2",true));
+ $i21 = array(3, "I21", $i21scenesAdjacentes, $item= array("brosse"));					
+
 $listeCases = array($g25, $couloir1, $couloir2, $i21);
 
-//----------------------------------------------------------------------------------------------------------------------------------
-
-
-//------------------------------------------------------------LES-OBJETS------------------------------------------------------------
-//----Le-crayon----
-		$tabAction1 = array("ecrit","colorié","detruit");
-		$description1="un crayon a papier normal";
-		$lienIMG1="images/crayon.jpg";
-		$tabInfos1 = array($tabAction1,$description1,$lienIMG1);
-	$tabItem2 = array("crayon",$tabInfos1);
+//----La-cle----
+                $tabAction1 = array("ouvrir porte");
+                $description1="Sert a ouvrir quelque chose";
+                $lienIMG1="images/cle.jpg";
+                $possession = false;
+                $tabInfos1 = array($tabAction1,$description1,$lienIMG1,$possession);
+        $tabItem2 = array("cle",$tabInfos1);
 
 //----La-pomme----
-		$tabAction2 = array("mangé","epluché","ecrasé");
-		$description2="une belle pomme rouge";
-		$lienIMG2="images/pomme.jpg";
-		$tabInfos2 = array($tabAction2,$description2,$lienIMG2);
-	$tabItem3 = array("pomme",$tabInfos2);
+                $tabAction2 = array("ecraser","manger");
+                $description2="une belle pomme rouge";
+                $lienIMG2="images/pomme.jpg";
+                $possession2 = false;
+                $tabInfos2 = array($tabAction2,$description2,$lienIMG2,$possession2);
+        $tabItem3 = array("pomme",$tabInfos2);
 
 //----La-brosse----	
-		$tabAction3 = array("effacé","depoussieré","jeté");
-		$description3="une brosse qui semble use par le temps";
-		$lienIMG3="images/brosse.jpg";
-		$tabInfos3 = array($tabAction3,$description3,$lienIMG3);
-	$tabItem4 = array("brosse",$tabInfos3);																			
+                $tabAction3 = array("brosser");
+                $description3="une brosse qui semble use par le temps";
+                $lienIMG3="images/brosse.jpg";
+                $possession3 = false;
+                $tabInfos3 = array($tabAction3,$description3,$lienIMG3,$possession3);
+        $tabItem4 = array("brosse",$tabInfos3);																			
 
 //---Répertoriassions-de-tous-les-items-dans-un-seul-tableau---	
-	$tabDeTousLesItems=array($tabItem2,$tabItem3,$tabItem4);
-//----------------------------------------------------------------------------------------------------------------------------------
-
+$tabDeTousLesItems=array($tabItem2,$tabItem3,$tabItem4);
 ?>
 
 
